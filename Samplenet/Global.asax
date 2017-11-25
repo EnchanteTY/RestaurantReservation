9686baedbfbf47eb4e897110e7c0835ce1218cad
Sharene <sharene21ty@outlook.com>
2017-11-25 11:04:30 +0800

    Add project files.

---
+++ Samplenet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Samplenet.MvcApplication" Language="C#" %>
---
+++ Samplenet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Samplenet.Startup))]
+namespace Samplenet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
